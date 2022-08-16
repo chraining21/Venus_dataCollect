4c5618090158d840c1da7cdd739a04ff8871a4e4
chraining21 <74174198+chraining21@users.noreply.github.com>
2022-08-16 09:27:15 +0800

    123

--- post.py
+++ post.py
@@ -1,6 +1,8 @@
 import requests
-url = 'http://127.0.0.1:8000/register'
+url = 'http://127.0.0.1:8000/upload'
+with open('./test.jpg', 'rb') as f:
+    img = f.read()
+file = {'file': ('test.jpg',img, 'image/jpg')}
+r=requests.post(url,files=file)
 
-r=requests.get(url)
-
-print(r)
+print(r.text)
